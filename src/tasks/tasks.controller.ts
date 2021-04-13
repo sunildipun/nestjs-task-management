a10f3955d41535e1f40bff2faf2bd9ca2306420e
sunildipun <sunil.6318ie@gmail.com>
2021-04-13 17:04:24 +0530

    update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -28,5 +28,10 @@ export class TasksController {
   @Delete('/:id')
   deleteTask(@Param('id') id: string): void {
     this.taskService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  patchTask(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    return this.taskService.updateTaskStatus(id, status);
   }
 }
